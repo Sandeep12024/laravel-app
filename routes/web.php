4896d4c239daf96ebf8b54d56e8a31fc1f8cbec9
Sandeep <sandeep730073@gmail.com>
2024-07-28 19:48:58 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\StudentsController;
 //     return view('welcome');
 // });
 
+
+/*
+# Single route
 Route::get('/', [StudentsController::class, 'allStudents'])->name('allStudent');
 
 Route::post('/add', [StudentsController::class, 'addStudent'])->name('addNewStudent');
@@ -18,3 +21,23 @@ Route::get('/update/{id}', [StudentsController::class, 'getSingleRecord'])->name
 Route::post('/updateData/{id}', [StudentsController::class, 'updateStudent'])->name('updateStudent');
 
 Route::get('/delete/{id}', [StudentsController::class, 'deleteStudent'])->name('deleteStudent');
+
+*/
+
+
+# Group route
+Route::controller(StudentsController::class)->group(function () {
+
+    Route::get('/', 'allStudents')->name('allStudent');
+
+    Route::post('/add', 'addStudent')->name('addNewStudent');
+    
+    Route::view('/add', '/crud.add_student');
+    
+    Route::get('/update/{id}', 'getSingleRecord')->name('getStudent');
+    
+    Route::post('/updateData/{id}', 'updateStudent')->name('updateStudent');
+    
+    Route::get('/delete/{id}', 'deleteStudent')->name('deleteStudent');
+
+});
